2bff3c98e4f3201f7d2fdfd0c4555424925e152e
Tashrik Anam <anampartho31@gmail.com>
2021-05-21 16:24:10 +0600

    Add base rendering

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 const addToCartBtns = document.querySelectorAll('.add-to-cart');
 const cart = document.getElementById('cart');
 const selectedProducts = {};
+const productsTableBody = document.getElementById('products-table-body');
+const totalPrice = document.getElementById('total-price');
 
 document.querySelector('body').addEventListener('click', function(event) {
   event.preventDefault();
@@ -19,4 +21,22 @@ document.querySelector('body').addEventListener('click', function(event) {
       };
     }
   }
+
+  renderCart();
 });
+
+function renderCart() {
+  let output = '';
+  for (const product in selectedProducts) {
+    output += `
+    <tr>
+      <td>${selectedProducts[product].name}</td>
+      <td>${selectedProducts[product].count} <a href="#" class="button is-small reduce-product"><span class="icon is-small"><i class="fas fa-minus" data-reduce="${product}"></i></span></a></td>
+      <td>$25</td>
+      <td><a href="#" class="button is-danger is-small remove-product" data-remove="${product}"><span class="icon is-small"><i class="fas fa-trash"></i></span></a></td>
+    </tr>
+    `;
+  }
+
+  productsTableBody.innerHTML = output;
+}
